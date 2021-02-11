2d12eee646d9a1aad8e2f475fc7e8d56a5d215a1
Rjal95 <54391774+Rjal95@users.noreply.github.com>
2021-02-11 09:36:44 +0000

    first

---
+++ src/www/calculatrice/service/Calculator.java
@@ -0,0 +1,19 @@
+package www.calculatrice.service;
+
+public class Calculator {
+	
+	public double sum (double x, double y) {
+		return x+y;
+	}
+	
+	public double dif (double x, double y) {
+		return x-y;
+	}
+	
+	public double prod (double x, double y) {
+		return x*y;
+	}
+
+
+
+}
